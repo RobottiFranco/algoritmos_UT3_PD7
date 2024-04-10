97e235b36519f8f899a7f5e8cbd410ca553c1ddf
RobottiFranco <franco.robotti0@gmail.com>
2024-04-10 18:04:02 -0300

    fix TAlumno

--- src/main/java/com/example/TAlumno.java
+++ src/main/java/com/example/TAlumno.java
@@ -1,7 +1,7 @@
 package com.example;
 
-public class TAlumno{
-    int cedula;
+public class TAlumno {
+    Integer cedula;
     String nombre;
     String apellido;
 
@@ -11,30 +11,34 @@ public class TAlumno{
         this.apellido = apellido;
     }
 
+    public Comparable<Integer> getComparable(){
+        return this.cedula;
+    }
+
     public static void main(String[] args) {
         // 1. Crear dos instancias del TDA Conjunto para representar los cursos
-        Conjunto<TAlumno> cursoAED1 = new Conjunto<TAlumno>();
-        Conjunto<TAlumno> cursoPF = new Conjunto<TAlumno>();
+        IConjunto<TAlumno> cursoAED1 = new Conjunto<TAlumno>();
+        IConjunto<TAlumno> cursoPF = new Conjunto<TAlumno>();
 
         // 2. Crear varias instancias de TAlumno
         TAlumno alumno1 = new TAlumno(1234, "Juan", "Perez");
         TAlumno alumno2 = new TAlumno(5678, "Maria", "Gomez");
         TAlumno alumno3 = new TAlumno(9012, "Pedro", "Martinez");
-/*
+
         // 3. Asignar alumnos a los cursos
-        cursoAED1.insertar(alumno1);
-        cursoAED1.insertar(alumno2);
-        cursoPF.insertar(alumno2); // Maria está en ambos cursos
-        cursoPF.insertar(alumno3);
+        cursoAED1.insertar(alumno1.getComparable(), alumno1);
+        cursoAED1.insertar(alumno2.getComparable(), alumno2);
+        cursoPF.insertar(alumno2.getComparable(), alumno2);
+        cursoPF.insertar(alumno3.getComparable(), alumno3);
 
         // 4. Listado de alumnos en cualquiera de los dos cursos
-        Conjunto<TAlumno> alumnosEnCualquierCurso = cursoAED1.union(cursoPF);
+        IConjunto<TAlumno> alumnosEnCualquierCurso = cursoAED1.union(cursoPF);
         System.out.println("Alumnos en cualquier curso:");
-        alumnosEnCualquierCurso.mostrarElementos();
+        alumnosEnCualquierCurso.imprimir(", ");
 
         // 5. Listado de alumnos en ambos cursos
-        Conjunto<TAlumno> alumnosEnAmbosCursos = cursoAED1.interseccion(cursoPF);
+        IConjunto<TAlumno> alumnosEnAmbosCursos = cursoAED1.interseccion(cursoPF);
         System.out.println("Alumnos en ambos cursos:");
-        alumnosEnAmbosCursos.mostrarElementos(); */
+        alumnosEnAmbosCursos.imprimir(", ");
     }
 }
